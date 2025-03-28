35cabd197536b6ca56a9e847dfd4fcb13be61e0d
Luis Tanafranca <80248146+ltanafranca1004@users.noreply.github.com>
2025-03-28 12:47:04 -0700

    Added Library Item Finder + simple css

--- modules/routes.py
+++ modules/routes.py
@@ -1,7 +1,7 @@
 # ----------------------------
 # Query logic & Requests
 # ----------------------------
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 import sqlite3
 
 def init_routes(app):
@@ -26,3 +26,22 @@ def init_routes(app):
         items = cur.fetchall()
         conn.close()
         return render_template('items.html', items=items)
+    
+    @app.route('/find', methods=['GET', 'POST'])
+    def find_item():
+        results = []
+        if request.method == 'POST':
+            search_query = request.form['title']
+            conn = sqlite3.connect('library.db')
+            cur = conn.cursor()
+            cur.execute('''
+                SELECT li.item_id, li.title, li.available_copies,
+                    pb.ISBN, pb.publisher, pb.author
+                FROM LibraryItems li
+                LEFT JOIN PrintBooks pb ON li.item_id = pb.item_id
+                WHERE li.title LIKE ?
+            ''', (f'%{search_query}%',))
+            results = cur.fetchall()
+            conn.close()
+        return render_template('find.html', results=results)
+
